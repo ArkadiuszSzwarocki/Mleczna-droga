c25275b41b58b39d4a24042b4cddc77c829f8434
ArkadiuszSzwarocki <dacklowicz@wp.pl>
2026-01-06 14:02:30 +0100

    06.01.2024, 14:02:56 +0000 dopisanie allowedHosts do konfiguracji serwera Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['filipinka.myqnapcloud.com'],
+    allowedHosts: ['filipinka.myqnapcloud.com', 'mlecznadroga.mycloudnas.com'],
     port: 5178,
     proxy: {
       '/api': {
