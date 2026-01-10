777d0d1b5755f42a09a1f370d65c547302a47b0e
ArkadiuszSzwarocki <dacklowicz@wp.pl>
2026-01-10 18:11:52 +0100

    10.01.2024, 12:34:56 +0000

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5178,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5001',
+        target: 'http://filipinka.myqnapcloud.com',
         changeOrigin: true,
       }
     }
